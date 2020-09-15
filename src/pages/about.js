95d5c80ec994e12cd16607e86132dae2d7c78a8d
Folajomi Shotunde <40181350+jormee@users.noreply.github.com>
2020-09-15 14:33:45 +0100

    merge about

--- src/pages/about.js
+++ src/pages/about.js
@@ -9,14 +9,14 @@ import '../styles/sharedComponents.scss'
 const About = () => {
   return(
     <Layout>
-      <SEO title="about me - Folajomi"/>
+      <SEO title="About Me"/>
       <div className="container about">
         <section className='section-header'>
           <img src={aboutImg} alt='about me' className='section-img' />
           <div className='page-description'>
             <h1 className='page-title'>About Me</h1>
             <div className='section-description'>
-              <p>Hey there <span role='image' alt="waving hand">👋</span>, I'm Folajomi Shotunde, Project Manager, Front-End Web Developer, Web Designer and compulsive brooder.</p>
+              <p>Hey there <span role='img' aria-labelledby="waving hand">👋</span>, I'm Folajomi Shotunde, Project Manager, Front-End Web Developer, Web Designer and compulsive brooder.</p>
               <p>I am particularly drawn towards artful and functional designs and work towards attaining wonderful projects and great user experience.</p>
               <p>When I am not designing/coding websites, I might be found brooding or analysing crypto markets (huge fan).</p>
             </div>
