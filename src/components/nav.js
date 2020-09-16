5d7d17cfe035e694e9956e7a65e112bc311c6e09
Folajomi Shotunde <40181350+jormee@users.noreply.github.com>
2020-09-16 22:46:48 +0100

    STYLE: Add color changes to nav-bar to respond to theme changes
    Resize logo image to reduce paddings to left of logo

--- src/components/nav.js
+++ src/components/nav.js
@@ -18,10 +18,11 @@ import '../styles/nav.scss'
 const Nav = ({ siteTitle }) => {
     const { sidebarOpen, toggleSidebar } = useContext(NavContext)
     const { isLightTheme, themeToggle } = useContext(ThemeContext)
+    const theme = isLightTheme ? 'light' : 'dark'
     const sidebar = sidebarOpen ? 'open' : ''
 
     return(
-        <Headroom>
+        <Headroom className={theme}>
             <header className="container header">
                 <div className="links">
                     <Link to="/" className="logo"><img alt="logo" src={isLightTheme ? logoLight : logoDark} /></Link>
@@ -32,36 +33,38 @@ const Nav = ({ siteTitle }) => {
                         <li className="social-icon"><a href='https://linkedin.com/in/folajomi-shotunde'><LinkedIn /></a></li>
                     </ul>
                 </div>
-                <nav className={`nav ${sidebar}`}>
-                    <ul className="nav-bar">
+                <div className="theme">
+                    <nav className={`nav ${sidebar}`}>
+                        <ul className="nav-bar">
 
-                        <li className="nav-item">
-                            <Link to="/" className="nav-link" activeClassName="active" onClick={() => toggleSidebar()}>home</Link>
-                        </li>
-                        <li className="nav-item">
-                            <Link to="/about" className="nav-link" activeClassName="active" onClick={() => toggleSidebar()}>about</Link>
-                        </li>
-                        <li className="nav-item">
-                            <Link to="/portfolio" className="nav-link" activeClassName="active" onClick={() => toggleSidebar()}>portfolio</Link>
-                        </li>
-                        <li className="nav-item">
-                            <a href="https://blog.folajomi.dev" target="_blank" rel="noopener noreferrer" className="nav-link" activeClassName="active">blog</a>
-                        </li>
-                        <li className="nav-item">
-                            <Link to="/talks" className="nav-link" activeClassName="active" onClick={() => toggleSidebar()}>talks</Link>
-                        </li>
+                            <li className="nav-item">
+                                <Link to="/" className="nav-link" activeClassName="active" onClick={() => toggleSidebar()}>home</Link>
+                            </li>
+                            <li className="nav-item">
+                                <Link to="/about" className="nav-link" activeClassName="active" onClick={() => toggleSidebar()}>about</Link>
+                            </li>
+                            <li className="nav-item">
+                                <Link to="/portfolio" className="nav-link" activeClassName="active" onClick={() => toggleSidebar()}>portfolio</Link>
+                            </li>
+                            <li className="nav-item">
+                                <a href="https://blog.folajomi.dev" target="_blank" rel="noopener noreferrer" className="nav-link" activeClassName="active">blog</a>
+                            </li>
+                            <li className="nav-item">
+                                <Link to="/talks" className="nav-link" activeClassName="active" onClick={() => toggleSidebar()}>talks</Link>
+                            </li>
 
-                    </ul>
-                </nav>
-                <div className="sidebar-toggle" onClick={toggleSidebar}>
-                    <div className="bar" id="1"></div>
-                    <div className="bar" id="2"></div>
-                    <div className="bar" id="3"></div>
-                </div>
-                <div className="theme-toggle">
-                    <button className="btn" onClick={() => themeToggle()}>
-                        {isLightTheme ? <Crescent /> : <Sun />}
-                    </button>
+                        </ul>
+                    </nav>
+                    <div className="sidebar-toggle" onClick={toggleSidebar}>
+                        <div className="bar" id="1"></div>
+                        <div className="bar" id="2"></div>
+                        <div className="bar" id="3"></div>
+                    </div>
+                    <div className="theme-toggle">
+                        <button className="btn" onClick={() => themeToggle()}>
+                            {isLightTheme ? <Crescent /> : <Sun />}
+                        </button>
+                    </div>
                 </div>
             </header>
         </Headroom>
