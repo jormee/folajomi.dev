973bbe1a05d80fc79f9cce9332f792b1c076f119
Folajomi Shotunde <40181350+jormee@users.noreply.github.com>
2020-10-28 17:25:00 +0100

    FEAT: Add 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -3,11 +3,20 @@ import React from "react"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
+import errorImage from "../images/404.png"
+
+import "../styles/sharedComponents.scss"
+import "../styles/404.scss"
+
 const NotFoundPage = () => (
   <Layout>
     <SEO title="404: Not found" />
-    <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    <div className="container">
+      <div className="error-div">
+        <img src={errorImage} alt="error" />
+        <p className="text">You just hit a route that doesn&#39;t exist.</p>
+      </div>
+    </div>
   </Layout>
 )
 
