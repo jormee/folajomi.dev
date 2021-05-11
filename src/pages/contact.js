58b6a1413ba837eddd2d8b05e8db2de6a3c1a4bf
Folajomi Shotunde <40181350+jormee@users.noreply.github.com>
2021-05-11 15:35:45 +0100

    STYLE: fix contact page styles

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -19,41 +19,43 @@ const IndexPage = () => {
       <SEO title="Contact Me" />
         <div className="container">
 
-          <section className='page-header'>
-            <h1 className='page-title'>Shoot me an Email</h1>
-            <div className='page-description'>
-              <p>Hey there <span role='img' aria-labelledby="waving hand">👋🏾</span>, would you like to work together? Shoot me an email by filling out the email form below <span role='img' aria-labelledby="pointing hand">👇🏾</span>.</p>
-            </div>         
-          </section>
-          
-          <form 
-            method = "post" 
-            action ="https://formspree.io/f/xrgrelrn" 
-            className="email-form"
-          >
-            <h1 className="form-header">Send an Email</h1>
-            <div className="text">
-              <input type="text" name="name" autoComplete="off" required/>
-              <span className="placeholder-icon"><User /></span>
-              <label htmlFor="name" className="placeholder">Name:</label>
-            </div>
-            <div className="text">
-              <input type="email" name="_replyto" autoComplete="off" required/>
-              <span className="placeholder-icon"><Mention /></span>
-              <label htmlFor="email" className="placeholder">Email:</label>
-            </div>
-            <div className="text">
-              <textarea type="textarea" name="message" autoComplete="off" required/>
-              <span className="placeholder-icon"><Mail /></span>
-              <label htmlFor="message" className="placeholder">Your Message:</label>
-            </div>
-            <input type="text" name="_gotcha" style={{display: "none"}} />
+          <div className="page">
+            <section className='page-header'>
+              <h1 className='page-title'>Shoot me an Email</h1>
+              <div className='page-description'>
+                <p>Hey there <span role='img' aria-labelledby="waving hand">👋🏾</span>, would you like to work together? Shoot me an email by filling out the email form below <span role='img' aria-labelledby="pointing hand">👇🏾</span>.</p>
+              </div>         
+            </section>
+            
+            <form 
+              method = "post" 
+              action ="https://formspree.io/f/xrgrelrn" 
+              className="email-form"
+            >
+              <h1 className="form-header">Send an Email</h1>
+              <div className="text">
+                <input type="text" name="name" autoComplete="off" required/>
+                <span className="placeholder-icon"><User /></span>
+                <label htmlFor="name" className="placeholder">Name:</label>
+              </div>
+              <div className="text">
+                <input type="email" name="_replyto" autoComplete="off" required/>
+                <span className="placeholder-icon"><Mention /></span>
+                <label htmlFor="email" className="placeholder">Email:</label>
+              </div>
+              <div className="text">
+                <textarea type="textarea" name="message" autoComplete="off" required/>
+                <span className="placeholder-icon"><Mail /></span>
+                <label htmlFor="message" className="placeholder">Your Message:</label>
+              </div>
+              <input type="text" name="_gotcha" style={{display: "none"}} />
 
-            <button className="btn btn-submit">
-              <p>Send</p>
-              <Send />
-            </button>
-          </form>
+              <button className="btn btn-submit">
+                <p>Send</p>
+                <Send />
+              </button>
+            </form>
+          </div>
         </div>
     </Layout>
   )
